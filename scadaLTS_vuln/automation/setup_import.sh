e7af83a629ad150b0d35dd235d4640d171727c4b
Hubert Golasa <vjescy@pm.me>
2026-01-15 18:43:51 +0100

    Removed duplciation of steps
    Removed steps for activating virtual environment and installing Playwright dependencies.

--- scadaLTS_vuln/automation/setup_import.sh
+++ scadaLTS_vuln/automation/setup_import.sh
@@ -44,24 +44,3 @@ python login_openplc.py
 
 echo "[✔] Done."
 
-# Step 2: Activate virtual environment
-echo "[+] Activating virtual environment..."
-source venv/bin/activate
-
-# Step 3: Upgrade pip and install dependencies
-echo "[+] Installing Playwright..."
-pip install --upgrade pip
-pip install playwright
-
-# Step 4: Install Playwright browsers
-playwright install
-
-# Step 5: Run the Python automation script
-echo "[+] Running SCADA-LTS login script..."
-python login_scada.py
-
-# Step 6: Run the Python automation script
-echo "[+] Running OPENPLC login script..."
-python login_openplc.py
-
-echo "[✔] Done."
